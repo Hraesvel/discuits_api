1dc7b45fc11d912c23c922706f38cfb4810b943e
Martin Smith <mcodesmith@gmail.com>
2020-11-12 12:49:32 -0800

    moved session into module file

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,46 +1,5 @@
-use std::sync::Arc;
-
-use async_trait::async_trait;
-
-use crate::engine::db::{Db, DbActions};
-
 pub mod file_system;
 pub mod db;
+pub mod session;
 
 pub(crate) type EngineError = Box<dyn std::error::Error + Sync + Send>;
-
-pub trait NewSession {}
-
-pub struct Session<T> {
-    engine: Arc<T>
-}
-
-impl<T> Session<T>
-{
-    pub fn from(t: T) -> Result<Session<T>, EngineError> {
-        Ok(Session { engine: Arc::new(t) })
-    }
-}
-
-
-#[async_trait]
-impl NewSession for Db {}
-
-
-#[cfg(test)]
-mod test {
-    use crate::engine::{EngineError, Session};
-    use crate::engine::db::{AuthType, Db};
-
-    #[tokio::test]
-    async fn test_session_db() -> Result<(), EngineError> {
-        let db =
-            Db::new()
-                .db_name("discket_dev")
-                .auth_type(AuthType::Jwt { user: "discket", pass: "babyYoda" })
-                .connect().await?;
-        let session: Session<Db> = Session::from(db)?;
-
-        Ok(())
-    }
-}
