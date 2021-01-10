880472fcebf52b5a4e6fbeb650eb8dd7f738ec79
Martin Smith <mcodesmith@gmail.com>
2021-01-10 12:24:09 -0800

    added Variant as module to models
    additionally remove redundant lifetime from `DocDetail::key()`

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -4,15 +4,20 @@ use crate::engine::db::arangodb::aql_snippet;
 use crate::engine::db::Db;
 use crate::engine::EngineError;
 use crate::io::read::EngineGet;
+use arangors::document::options::InsertOptions;
 
 pub mod album;
 pub mod artist;
 pub mod inventory;
+pub mod variant;
+
 
 pub trait RequiredTraits: serde::de::DeserializeOwned + serde::ser::Serialize + DocDetail + Sync + Send {}
 
 pub trait DocDetail {
     fn collection_name<'a>() -> &'a str;
 
-    fn key<'a>(&self) -> String;
+    fn key(&self) -> String;
+
+    fn id(&self) -> String;
 }
