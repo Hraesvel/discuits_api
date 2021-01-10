cbbb2b50c151d92c7c67b1650942ac2583008949
Martin Smith <mcodesmith@gmail.com>
2021-01-10 12:25:05 -0800

    Update to Inventory

--- src/models/inventory.rs
+++ src/models/inventory.rs
@@ -2,16 +2,49 @@ use std::borrow::Cow;
 
 use arangors::document::options::InsertOptions;
 use async_trait::async_trait;
+use uuid::Uuid;
 
 use crate::engine::db::Db;
 use crate::engine::EngineError;
 use crate::io::write::Write;
+use crate::models::DocDetail;
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Inventory {
     _id: Cow<'static, str>,
     _key: Cow<'static, str>,
     count: u8,
+}
+
+impl Inventory {
+    pub fn new() -> Self {
+        let uid = Uuid::new_v4().to_string()[0..8].to_string();
+        Inventory {
+            _key: Cow::from(uid),
+            ..Inventory::default()
+        }
+    }
+
+    pub fn amount(&mut self, count: u8) -> &mut Self {
+        self.count = count;
+        self
+    }
+}
+
+impl DocDetail for Inventory {
+    /// Returns data type name used by DB.
+    /// Helper function to  avoid hard coding a collection's name in business logic code
+    fn collection_name<'a>() -> &'a str {
+        "inventory"
+    }
+
+    fn key(&self) -> String {
+        self._key.to_string()
+    }
+
+    fn id(&self) -> String {
+        format!("{}/{}", Self::collection_name(), self._key)
+    }
 }
 
 #[async_trait]
