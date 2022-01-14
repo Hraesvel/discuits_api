c7a57c2e09beaf12124e3ca37ed1adae0862b7e3
Martin Smith <mcodesmith@gmail.com>
2022-01-13 17:52:50 -0800

    updating test server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,11 @@
-use actix_web::web::{self, Data};
-use actix_web::{App, HttpServer, Responder};
+use actix_web::web::{self, get, Data};
+use actix_web::{App, HttpResponse, HttpServer, Responder};
+use std::ops::Deref;
 
 use discuits_api::engine::db::{ArangoDb, AuthType, Db, DbBasics, DbBuilder};
 use discuits_api::engine::session::Session;
-use discuits_api::engine::EngineError;
-use discuits_api::io::Get;
+use discuits_api::engine::{DbError, EngineError};
+use discuits_api::io::read::*;
 use discuits_api::models::album::Album;
 
 #[actix_web::main]
@@ -17,7 +18,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(session.clone())
-            .service(web::scope("/app").route("", web::get().to(hello)))
+            .service(web::scope("/app").route("", get().to(get_all_albums)))
     })
     .bind("127.0.0.1:8181")?
     .run()
@@ -48,10 +49,18 @@ async fn config_database_2() -> Result<Session<Db<ArangoDb>>, EngineError> {
     Ok(Session::new(db))
 }
 
-async fn hello(data: Data<Session<Db<ArangoDb>>>) -> impl Responder {
-    let reader = data.db().read().await;
-    let a = Album::get_all(&reader).await;
-    let json =
-        serde_json::to_string::<Album>(&a.unwrap()[0]).unwrap_or_else(|_| "failed to parse".to_string());
-    format!("Using Database: {:?}", json)
+async fn get_all_albums(data: Data<Session<Db<ArangoDb>>>) -> actix_web::Result<HttpResponse> {
+    let db = data.db().read().await;
+    let a = db.get_all::<Album>().await.map_err(|err| {
+        if let Some(db_error) = err.downcast_ref::<DbError>() {
+            match db_error {
+                DbError::ItemNotFound => actix_web::error::ErrorNotFound("Not found"),
+                _ => actix_web::error::ErrorInternalServerError("Whoops"),
+            }
+        } else {
+            actix_web::error::ErrorInternalServerError("Whoops")
+        }
+    })?;
+
+    Ok(HttpResponse::Ok().json(a))
 }
